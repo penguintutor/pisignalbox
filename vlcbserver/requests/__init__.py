b0ba76caaf683276e7cbbee7dc647d7696cacac4
Stewart Watkiss (penguintutor) <5587863+penguintutor@users.noreply.github.com>
2026-08-31 10:35:06 +0100

    Move css into separate file and change static url to resources

--- vlcbserver/requests/__init__.py
+++ vlcbserver/requests/__init__.py
@@ -14,7 +14,7 @@ web_blueprint = Blueprint(
     __name__, 
     template_folder='templates',    # Template files are processed by Jinja2 - allows {{ var_name }}
     static_folder='static',         # Files in the static directory are served as is - eg. CSS / JS
-    static_url_path='/web-static'   # Uses a unique URL
+    static_url_path='/resources'   # Uses a unique URL
 )
 
 # Import routes at the bottom so they attach to the blueprints above
