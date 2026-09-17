86cf9a1c5fe3ca0359dce9dffa9b8ceb93bdc6ab
Stewart Watkiss (penguintutor) <5587863+penguintutor@users.noreply.github.com>
2026-09-17 22:11:38 +0100

    Update security tests for new password reset

--- vlcbserver/tests/test_security.py
+++ vlcbserver/tests/test_security.py
@@ -14,6 +14,8 @@ def test_all_routes_are_secure(app):
         'auth.login', 
         'auth.register', 
         'auth.logout',
+        'auth.reset_request',
+        'auth.reset_token',
         'home.home', 
         'static'  # Flask's default static file handler
     ]
